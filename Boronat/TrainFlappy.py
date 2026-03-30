c3215cec00319e93f6aff9f7edf342e6beadf946
Youghz <boronathugo@gmail.com>
2026-03-29 22:35:44 -0400

    Refactor TrainFlappy: clean code, ReplayBuffer class, training loop

--- Boronat/TrainFlappy.py
+++ Boronat/TrainFlappy.py
@@ -1,239 +1,198 @@
-from ple.games.flappybird import FlappyBird
-from ple import PLE
-import numpy as np
+"""Deep Q-Network training for Flappy Bird.
+
+Trains a CNN-based DQN agent using experience replay on the
+PyGame Learning Environment (PLE) Flappy Bird game.
+"""
+
 import numpy as np
 from collections import deque
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten
+from keras.layers import Conv2D, Dense, Flatten
+from keras.optimizers import Adam
+from ple import PLE
+from ple.games.flappybird import FlappyBird
 from skimage.color import rgb2gray
 from skimage.transform import resize
-import skimage
-from keras.optimizers import SGD , Adam
-import matplotlib.pyplot as plt
-import random
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.core import Dense, Dropout, Activation, Flatten
 
-print("TrainFlappy")
+# --- Hyperparameters ---
 
-def build_dqn():
-	dqn = Sequential()
-	#1st layer
-	dqn.add(Conv2D(filters=16, kernel_size=(8,8), strides=4, activation="relu", input_shape=(80,80,4)))
-	#2nd layer
-	dqn.add(Conv2D(filters=32, kernel_size=(4,4), strides=2, activation="relu"))
-	dqn.add(Flatten())
-	#3rd layer
-	dqn.add(Dense(units=256, activation="relu"))
-	#output layer
-	dqn.add(Dense(units=2, activation="linear"))
-	adam = Adam(lr=1e-4)
-	dqn.compile(optimizer=adam, loss="mean_squared_error")
-	return dqn
+FRAME_SIZE = (80, 80)
+STACK_SIZE = 4
+TOTAL_STEPS = 1_000_000
+REPLAY_MEMORY_SIZE = 1_000_000
+MINI_BATCH_SIZE = 32
+GAMMA = 0.99
+LEARNING_RATE = 1e-4
+EVAL_PERIOD = 10_000
+SAVE_PERIOD = 50_000
+ACTIONS = None  # Set after PLE init
 
-def policy_eval(p, games, network):
-    """
-    Equivalnt to MonteCarlo Eval in RL notebook
-    """
-    list_actions = p.getActionSet()
-    cumulated = np.zeros((games))
-    for i in range(games):
-        stackedframes = deque([np.zeros((80,80)),np.zeros((80,80)),np.zeros((80,80)),np.zeros((80,80))], maxlen=4)
-        p.reset_game()
-        while(not p.game_over()):
-            screen = process_screen(p.getScreenRGB())
-            stackedframes.append(screen)
-            frameStacked = np.stack(stackedframes, axis=-1)
-            action = list_actions[np.argmax(network.predict(np.expand_dims(frameStacked,axis=0)))]
-            reward = p.act(action)
-            cumulated[i] += reward
-    mean_score = np.mean(cumulated)
-    max_score = np.max(cumulated)
-    return mean_score, max_score
 
-class MemoryBuffer:
-    "An experience replay buffer using numpy arrays"
-    def __init__(self, length, screen_shape, action_shape):
-        self.length = length
-        self.screen_shape = screen_shape
-        self.action_shape = action_shape
-        shape = (length,) + screen_shape
-        self.screens_x = np.zeros(shape, dtype=np.uint8) # starting states
-        self.screens_y = np.zeros(shape, dtype=np.uint8) # resulting states
-        shape = (length,) + action_shape
-        self.actions = np.zeros(shape, dtype=np.uint8) # actions
-        self.rewards = np.zeros((length,1), dtype=np.uint8) # rewards
-        self.terminals = np.zeros((length,1), dtype=np.bool) # true if resulting state is terminal
+# --- Model ---
+
+
+def build_dqn() -> Sequential:
+    """Build the CNN architecture for Q-value estimation."""
+    model = Sequential([
+        Conv2D(16, (8, 8), strides=4, activation="relu", input_shape=(80, 80, 4)),
+        Conv2D(32, (4, 4), strides=2, activation="relu"),
+        Flatten(),
+        Dense(256, activation="relu"),
+        Dense(2, activation="linear"),
+    ])
+    model.compile(optimizer=Adam(learning_rate=LEARNING_RATE), loss="mse")
+    return model
+
+
+# --- Experience Replay ---
+
+
+class ReplayBuffer:
+    """Fixed-size circular buffer storing (state, action, reward, next_state, done) transitions."""
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.screens_x = np.zeros((capacity, *FRAME_SIZE), dtype=np.uint8)
+        self.screens_y = np.zeros((capacity, *FRAME_SIZE), dtype=np.uint8)
+        self.actions = np.zeros((capacity, 1), dtype=np.uint8)
+        self.rewards = np.zeros((capacity, 1), dtype=np.uint8)
+        self.terminals = np.zeros((capacity, 1), dtype=bool)
         self.terminals[-1] = True
-        self.index = 0 # points one position past the last inserted element
-        self.size = 0 # current size of the buffer
-    
-    def append(self, screenx, a, r, screeny, d):
-        self.screens_x[self.index] = screenx
-        #plt.imshow(screenx)
-        #plt.show()
-        #plt.imshow(self.screens_x[self.index])
-        #plt.show()
-        self.actions[self.index] = a
-        self.rewards[self.index] = r
-        self.screens_y[self.index] = screeny
-        self.terminals[self.index] = d
-        self.index = (self.index+1) % self.length
-        self.size = np.min([self.size+1,self.length])
-    
-    def stacked_frames_x(self, index):
-        im_deque = deque(maxlen=4)
-        pos = index % self.length
-        for i in range(4): # todo
-            im = self.screens_x[pos]
-            im_deque.appendleft(im)
-            test_pos = (pos-1) % self.length
-            if self.terminals[test_pos] == False:
-                pos = test_pos
-        return np.stack(im_deque, axis=-1)
-    
-    def stacked_frames_y(self, index):
-        im_deque = deque(maxlen=4)
-        pos = index % self.length
-        for i in range(4): # todo
-            im = self.screens_y[pos]
-            im_deque.appendleft(im)
-            test_pos = (pos-1) % self.length
-            if self.terminals[test_pos] == False:
-                pos = test_pos
-        return np.stack(im_deque, axis=-1)
-    
-    def minibatch(self, size):
-        #return np.random.choice(self.data[:self.size], size=sz, replace=False)
-        indices = np.random.choice(self.size, size=size, replace=False)
-        x = np.zeros((size,)+self.screen_shape+(4,))
-        y = np.zeros((size,)+self.screen_shape+(4,))
-        for i in range(size):
-            x[i] = self.stacked_frames_x(indices[i])
-            y[i] = self.stacked_frames_y(indices[i])
+        self.index = 0
+        self.size = 0
+
+    def append(self, screen_x, action, reward, screen_y, done):
+        self.screens_x[self.index] = screen_x
+        self.actions[self.index] = action
+        self.rewards[self.index] = reward
+        self.screens_y[self.index] = screen_y
+        self.terminals[self.index] = done
+        self.index = (self.index + 1) % self.capacity
+        self.size = min(self.size + 1, self.capacity)
+
+    def _get_stacked_frames(self, screens, index):
+        """Build a 4-frame stack walking backwards from index, stopping at terminal states."""
+        frames = deque(maxlen=STACK_SIZE)
+        pos = index % self.capacity
+        for _ in range(STACK_SIZE):
+            frames.appendleft(screens[pos])
+            prev = (pos - 1) % self.capacity
+            if not self.terminals[prev]:
+                pos = prev
+        return np.stack(frames, axis=-1)
+
+    def sample(self, batch_size: int):
+        """Sample a random minibatch of transitions with stacked frames."""
+        indices = np.random.choice(self.size, size=batch_size, replace=False)
+        x = np.zeros((batch_size, *FRAME_SIZE, STACK_SIZE))
+        y = np.zeros((batch_size, *FRAME_SIZE, STACK_SIZE))
+        for i, idx in enumerate(indices):
+            x[i] = self._get_stacked_frames(self.screens_x, idx)
+            y[i] = self._get_stacked_frames(self.screens_y, idx)
         return x, self.actions[indices], self.rewards[indices], y, self.terminals[indices]
 
-# Function to display X and Y from minibatch to visualize what is stored in memory        
-def show_X_Y(X,Y):
-    temp = np.transpose(X[0],(2,1,0))
-    tempy = np.transpose(Y[0],(2,1,0))
 
-    plt.figure(figsize=(30,30))
-    plt.subplot(2,4,1)
-    plt.imshow(temp[0][:][:])
-    plt.subplot(2,4,2)
-    plt.imshow(temp[1][:][:])
-    plt.subplot(2,4,3)
-    plt.imshow(temp[2][:][:])
-    plt.subplot(2,4,4)
-    plt.imshow(temp[3][:][:])
-    plt.title(A[0])
-    plt.legend(R[0])
+# --- Utilities ---
 
-    plt.subplot(2,4,5)
-    plt.imshow(tempy[0][:][:])
-    plt.subplot(2,4,6)
-    plt.imshow(tempy[1][:][:])
-    plt.subplot(2,4,7)
-    plt.imshow(tempy[2][:][:])
-    plt.subplot(2,4,8)
-    plt.imshow(tempy[3][:][:])
-    plt.show()
 
-def epsilon(step):
-    if step<(1e6+99999):
-        return (1.-step*9e-7)*0.1
+def preprocess_frame(screen: np.ndarray) -> np.ndarray:
+    """Convert raw RGB screen to 80x80 grayscale."""
+    return 255 * resize(rgb2gray(screen[60:, 25:310, :]), FRAME_SIZE)
+
+
+def epsilon_schedule(step: int) -> float:
+    """Linearly decay epsilon from 0.1 to 0.001 over 1M steps."""
+    if step < 1_100_000:
+        return (1.0 - step * 9e-7) * 0.1
     return 0.001
 
-def clip_reward(r):
-    if r!=1:
-        rr=0.1 
-    else:
-        rr=r
-    return rr
+
+def clip_reward(reward: float) -> float:
+    """Clip reward: 1.0 for scoring, 0.1 otherwise (survival bonus)."""
+    return reward if reward == 1 else 0.1
 
 
-def greedy_action(network, x):
-    Q = network.predict(np.array([x]))
-    return np.argmax(Q)
+def greedy_action(model, state: np.ndarray) -> int:
+    """Select the action with highest Q-value."""
+    q_values = model.predict(np.array([state]))
+    return np.argmax(q_values)
 
-def process_screen(x):
-    return 255*resize(rgb2gray(x[60:, 25:310,:]),(80,80))
 
-game = FlappyBird(graphics="fixed") # use "fancy" for full background, random bird color and random pipe color, use "fixed" (default) for black background and constant bird and pipe colors.
-p = PLE(game, fps=30, frame_skip=1, num_steps=1, force_fps=True,display_screen=True)
-p.init()
+def evaluate(env: PLE, model, num_games: int = 10):
+    """Evaluate the current model over multiple games."""
+    scores = np.zeros(num_games)
+    for i in range(num_games):
+        frame_stack = deque([np.zeros(FRAME_SIZE)] * STACK_SIZE, maxlen=STACK_SIZE)
+        env.reset_game()
+        while not env.game_over():
+            screen = preprocess_frame(env.getScreenRGB())
+            frame_stack.append(screen)
+            stacked = np.stack(frame_stack, axis=-1)
+            action = ACTIONS[np.argmax(model.predict(np.expand_dims(stacked, axis=0)))]
+            scores[i] += env.act(action)
+    return np.mean(scores), np.max(scores)
 
-print("Test creation model")
-model = build_dqn()
-print("End creation model")
 
-total_steps = 1000000
-replay_memory_size = 1000000
-mini_batch_size = 32
-gamma = 0.99
+# --- Training Loop ---
 
- # initialize state and replay memory
-p.reset_game()
-screen_x = process_screen(p.getScreenRGB())
-stacked_x = deque([screen_x, screen_x, screen_x, screen_x], maxlen=4)
-x = np.stack(stacked_x, axis=-1)
 
-replay_memory = MemoryBuffer(replay_memory_size, (80, 80), (1,))
-evaluation_period = 10000
-frame_per_action = 1
-score =0
+def train():
+    global ACTIONS
 
-list_actions = p.getActionSet()
+    game = FlappyBird(graphics="fixed")
+    env = PLE(game, fps=30, frame_skip=1, num_steps=1, force_fps=True, display_screen=True)
+    env.init()
+    ACTIONS = env.getActionSet()
 
-# Deep Q-learning with experience replay
-for step in range(total_steps*frame_per_action):
+    model = build_dqn()
+    print("Model created")
 
-    if (step%frame_per_action == 0):
+    # Initialize replay buffer and first state
+    replay = ReplayBuffer(REPLAY_MEMORY_SIZE)
+    env.reset_game()
+    screen_x = preprocess_frame(env.getScreenRGB())
+    frame_stack = deque([screen_x] * STACK_SIZE, maxlen=STACK_SIZE)
+    state = np.stack(frame_stack, axis=-1)
 
-    	if np.random.rand() < epsilon(step):
-            a_test= np.random.randint(0,5)
-            if (a_test==1):
-            	a=1
-            else:
-            	a=0
+    for step in range(TOTAL_STEPS):
+        # Epsilon-greedy action selection (biased toward no-flap)
+        if np.random.rand() < epsilon_schedule(step):
+            action_idx = 1 if np.random.randint(0, 5) != 1 else 0
         else:
-            a = greedy_action(model,x)
+            action_idx = greedy_action(model, state)
 
-        action = list_actions[a]
-        r = clip_reward(p.act(action))
-        raw_screen_y = p.getScreenRGB()
-        screen_y = process_screen(raw_screen_y)
+        reward = clip_reward(env.act(ACTIONS[action_idx]))
+        screen_y = preprocess_frame(env.getScreenRGB())
+        done = env.game_over()
 
-        #Display image to check if cropped correctly and consistency with game
-        #plt.imshow(screen_y)
-        #plt.show()
+        replay.append(screen_x, action_idx, reward, screen_y, done)
 
-        d = p.game_over()
+        # Train after warmup period
+        if step > EVAL_PERIOD:
+            x_batch, a_batch, r_batch, y_batch, d_batch = replay.sample(MINI_BATCH_SIZE)
+            q_next = model.predict(y_batch)
+            q_max = q_next.max(axis=1).reshape((MINI_BATCH_SIZE, 1))
+            targets = r_batch + GAMMA * (1 - d_batch) * q_max
+            q_current = model.predict(x_batch)
+            q_current[np.arange(MINI_BATCH_SIZE), a_batch.ravel()] = targets.ravel()
+            model.train_on_batch(x=x_batch, y=q_current)
 
-        replay_memory.append(screen_x, a, r, screen_y, d)
-        
-        if step>evaluation_period*frame_per_action:
-            X,A,R,Y,D = replay_memory.minibatch(mini_batch_size)
-            QY = model.predict(Y)
-            QYmax = QY.max(1).reshape((mini_batch_size,1))
-            update = R + gamma * (1-D) * QYmax
-            QX = model.predict(X)
-            QX[np.arange(mini_batch_size), A.ravel()] = update.ravel()
-            model.train_on_batch(x=X, y=QX)
-
-        if d==True:
-            p.reset_game()
-            #score = 0
-            screen_x = process_screen(p.getScreenRGB())
-            stacked_x = deque([screen_x, screen_x, screen_x, screen_x], maxlen=4)
-            x = np.stack(stacked_x, axis=-1)
+        # Reset on game over, otherwise advance state
+        if done:
+            env.reset_game()
+            screen_x = preprocess_frame(env.getScreenRGB())
+            frame_stack = deque([screen_x] * STACK_SIZE, maxlen=STACK_SIZE)
         else:
-            # keep going
             screen_x = screen_y
-            stacked_x.append(screen_x)
-            x = np.stack(stacked_x, axis=-1)
+            frame_stack.append(screen_x)
+        state = np.stack(frame_stack, axis=-1)
 
-    if (step%50000*frame_per_action == 0):
-    	print(str(step)+str(policy_eval(p,10,model)))
-        model.save("FlappyModel_"+str(step))
+        # Periodic evaluation and model save
+        if step % SAVE_PERIOD == 0:
+            mean_score, max_score = evaluate(env, model)
+            print(f"Step {step}: mean={mean_score:.1f}, max={max_score:.1f}")
+            model.save(f"FlappyModel_{step}")
+
+
+if __name__ == "__main__":
+    train()
